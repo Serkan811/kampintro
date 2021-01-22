a86d8c2d631e8a4b7fb31ef65cab08b38a97f50a
Serkan Cebecioglu <1190605050@nku.edu.tr>
2021-01-22 23:48:19 +0300

    ÖDEV

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    
+        class Dictionary<T1, T2>
+        {
+            T1[] keys;
+            T2[] values;
+            public Dictionary()
+            {
+                keys = new T1[0];
+                values = new T2[0]; 
+            }
+
+            public void Add(T1 key, T2 value)
+            {
+
+                T1[] _tempKeys = keys;
+                T2[] _tempValues = values;
+
+                keys = new T1[keys.Length + 1];
+                values = new T2[values.Length + 1];
+
+                for (int i = 0; i < _tempKeys.Length; i++)
+                {
+                    keys[i] = _tempKeys[i];
+                    values[i] = _tempValues[i];
+                }
+
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }
+        
+        
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+       
+        public T1[] Keys
+            {
+                get { return keys; }
+            }
+
+            public T2[] Values
+            {
+                get { return values; }
+            }
+
+        }
+}
+
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> games = new Dictionary<int, string>();
+
+            games.Add(01, "Counter-Strike: Global Offensive");
+            games.Add(02, "League of Legends");
+            games.Add(03, "PlayerUnkown’s Battlegrounds");
+            games.Add(04, "Grand Theft Auto V");
+            games.Add(05, "Among Us");
+            games.Add(06, "Apex Legends");
+            games.Add(07, "Tom Clancy’s Rainbow Six: Siege");
+            games.Add(08, "Call of Duty: Modern Warfare");
+            games.Add(09, "Assassin’s Creed: Odyssey");
+            games.Add(10, "FIFA 2020");
+
+
+            Console.WriteLine("Dictionary Size: " + games.Count);
+
+            for (int i = 0; i < games.Count; i++)
+            {
+                Console.WriteLine(games.Keys[i] + " - " + games.Values[i]);
+            }
+        }
+    }
+}
+    
